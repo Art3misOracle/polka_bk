382d4d64dd2d9bbf2ca4d5901d548cfcbfd0ebc2
linus <1748501327@qq.com>
2024-10-21 17:14:30 +0800

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-        };
-      }
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  ssr: false,
+};
+
+export default nextConfig;
